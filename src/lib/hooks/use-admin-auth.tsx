077c6a614cbd11675f64bc4add3fa47f1702ab08
eeennsu <diso592@naver.com>
2024-08-15 15:46:18 +0900

    fix: Fixed not rendered toast when unauthenticated

--- src/lib/hooks/use-admin-auth.tsx
+++ src/lib/hooks/use-admin-auth.tsx
@@ -18,25 +18,27 @@ export const useAdminAuth = ({ isProtectedRoute = false, options = { required: t
     const { data: session, status } = useSession({
         ...options,
         onUnauthenticated: () => {
-            isProtectedRoute && router.replace(routePaths.home())
+            if (isProtectedRoute) {
+                router.replace(routePaths.home())
+
+                setTimeout(
+                    () =>
+                        toast({
+                            title: '접근 권한이 없습니다.',
+                            description: '관리자 권한이 필요합니다.',
+                            variant: 'secondary',
+                            className: cn(
+                                'fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:right-8 sm:top-8 sm:flex-col md:max-w-[220px]'
+                            ),
+                            duration: 3000,
+                        }),
+                    100
+                )
+            }
         },
     })
 
     const isAdminAuthed = status === 'authenticated' && session?.user?.isAdmin === true && session.expires
-
-    useEffect(() => {
-        if (isAdminAuthed === false && isProtectedRoute) {
-            toast({
-                title: '접근 권한이 없습니다.',
-                description: '관리자 권한이 필요합니다.',
-                variant: 'secondary',
-                className: cn(
-                    'fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:right-8 sm:top-8 sm:flex-col md:max-w-[220px]'
-                ),
-                duration: 3000,
-            })
-        }
-    }, [isAdminAuthed, isProtectedRoute])
 
     return { isAdminAuthed }
 }
