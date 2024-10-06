59a395b3c04ac0da97db458af54e92316982ea0c
eeennsu <diso592@naver.com>
2024-10-06 16:22:33 +0900

    feat: Implement edit post list order style

--- src/app/post/temporary-list/page.tsx
+++ src/app/post/temporary-list/page.tsx
@@ -12,8 +12,9 @@ const TemporaryListPage: FC = async () => {
     const temporarySavedPosts = response.posts
 
     return (
-        <main className='mx-auto  w-full max-w-4xl'>
-            <section className='flex flex-col gap-4 py-10 items-center'>
+        <main className='page-container mx-auto w-full gap-3 max-w-4xl pt-28 pb-8'>
+            <h2 className='text-2xl font-bold text-gray-100 mb-6'>임시 저장된 포스트</h2> {/* 리스트 제목 추가 */}
+            <div className='flex flex-col gap-6'>
                 {temporarySavedPosts.map((post) => (
                     <TemporarySavedPostItem
                         key={post.id}
@@ -23,7 +24,7 @@ const TemporaryListPage: FC = async () => {
                         createdAt={post.createdAt.toString()}
                     />
                 ))}
-            </section>
+            </div>
         </main>
     )
 }
--- src/features/post/temporary-saved/temporary-saved-post-item.tsx
+++ src/features/post/temporary-saved/temporary-saved-post-item.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { formatBeforeTime } from '@/lib/utils'
 import type { FC } from 'react'
 import { DeleteButton } from './delete-button'
@@ -17,10 +15,10 @@ export const TemporarySavedPostItem: FC<Props> = ({ id, title, summary, createdA
     return (
         <Link
             href={mainPath.post.create(`id=${id}`)}
-            className='inline-flex flex-col w-full gap-3 rounded-xl px-4 py-2 bg-blue-100'>
-            <h3 className='text-3xl font-semibold py-3'>{title}</h3>
-            <p className='line-clamp-3'>{summary}</p>
-            <div className='flex w-full justify-between items-center'>
+            className='flex flex-col w-full gap-4 rounded-lg bg-gray-800 p-5 shadow-lg hover:bg-gray-700 transition-all duration-300'>
+            <h3 className='text-xl font-semibold text-gray-100'>{title}</h3>
+            <p className='text-gray-400 line-clamp-3'>{summary}</p>
+            <div className='flex w-full justify-between items-center mt-4'>
                 <time className='text-sm text-gray-500'>{formatBeforeTime(createdAt)}</time>
                 <DeleteButton postId={id} />
             </div>
