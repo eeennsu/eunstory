abe97a5c2845f4acfa9809638898b01ec1442d77
eeennsu <diso592@naver.com>
2024-10-13 17:46:06 +0900

    fix: Remove api url

--- src/lib/fetch/api-url.ts
+++ src/lib/fetch/api-url.ts
@@ -1,6 +1,6 @@
 import { assertValue } from '../utils'
 
-const url = assertValue(process.env.VERCEL_ENV === 'production' ? process.env.API_URL : process.env.NEXT_PUBLIC_API_URL)
+const url = assertValue(process.env.NEXT_PUBLIC_API_URL)
 
 export const getUrlFromServer = (path: string) => {
     return `${url}${path}`
