41ab37c162143921f8c190bf2d8ce79a4fad1f15
eeennsu <diso592@naver.com>
2024-10-13 16:37:46 +0900

    fix: Fixed api url

--- src/lib/fetch/api-url.ts
+++ src/lib/fetch/api-url.ts
@@ -1,6 +1,6 @@
 import { assertValue } from '../utils'
 
-const url = assertValue(process.env.NEXT_PUBLIC_API_URL)
+const url = assertValue(process.env.VERCEL_ENV === 'preview' ? process.env.NEXT_PUBLIC_API_URL : process.env.API_URL)
 
 export const getUrlFromServer = (path: string) => {
     return `${url}${path}`
