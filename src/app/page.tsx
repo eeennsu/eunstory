bba67a476d4d3fc1081ce07698fb710c8ec211d8
eeennsu <diso592@naver.com>
2024-10-16 21:59:46 +0900

    build test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { serverRequestGetPostTagList } from '@/entities/post'
 import { FC } from 'react'
 
-console.log('NEXTAUTH_URL:', process.env.NEXTAUTH_URL)
-console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL)
-console.log('DATABASE_URL:', process.env.DATABASE_URL)
-
 const HomePage: FC = async () => {
+    const res = await serverRequestGetPostTagList()
+
+    console.log('tgas!!!!!!', res)
+
     return <main className='page-container pt-32 pb-10'></main>
 }
 
