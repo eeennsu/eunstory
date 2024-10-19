91c7430fa9fbc53b382a83796990f0d0e17efb1e
eeennsu <diso592@naver.com>
2024-10-19 10:20:14 +0900

    test

--- src/widgets/post/list/post-list-body-widget.tsx
+++ src/widgets/post/list/post-list-body-widget.tsx
@@ -1,12 +1,27 @@
-import { serverRequestGetSomePostList } from '@/entities/post'
+'use client'
+
+import { PostListItem } from '@/app/api/post/route'
+import { requestGetPostList, serverRequestGetSomePostList } from '@/entities/post'
 import { PostListByScroll } from '@/features/post/list/post-list-by-scroll'
-import type { FC } from 'react'
+import { useAsync } from '@/lib/hooks'
+import { useEffect, useState, type FC } from 'react'
 
 export const PostListBodyWidget: FC = async () => {
-    const responsePostList = await serverRequestGetSomePostList({ curPage: 1, perPage: 5, isPublished: true })
-    if ('error' in responsePostList) {
-        throw responsePostList.error
-    }
+    const [initialPosts, setInitialPosts] = useState<PostListItem[]>([])
 
-    return <PostListByScroll initialPosts={responsePostList?.posts} />
+    const { isLoading } = useAsync(async () => {
+        const responsePostList = await requestGetPostList({ curPage: 1, perPage: 5, isPublished: true })
+        if ('error' in responsePostList) {
+            throw responsePostList.error
+        }
+
+        setInitialPosts(responsePostList.posts)
+    }, [])
+
+    // const responsePostList = await serverRequestGetSomePostList({ curPage: 1, perPage: 5, isPublished: true })
+    // if ('error' in responsePostList) {
+    //     throw responsePostList.error
+    // }
+
+    return !isLoading && <PostListByScroll initialPosts={initialPosts} />
 }
