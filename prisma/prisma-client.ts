b277369408f7be354d1611d8a61633ec9d8e6332
eeennsu <diso592@naver.com>
2024-10-19 20:14:54 +0900

    fix: Update prisma client

--- prisma/prisma-client.ts
+++ prisma/prisma-client.ts
@@ -1,37 +1,33 @@
 import { PrismaClient } from '@prisma/client'
 import { createDefaultAdmin } from './initialize-data'
 
-const prismaClientSingleton = () => {
-    const prisma = new PrismaClient()
-
-    const setInitializeData = async () => {
-        await createDefaultAdmin(prisma)
-        return true
-    }
-
-    setInitializeData()
-        .then(() => {
-            // eslint-disable-next-line no-console
-            console.log('🚀  Prisma is ready')
-        })
-        .catch((error) => {
-            // eslint-disable-next-line no-console
-            console.log('❌  Failed to ready about prisma')
-            // eslint-disable-next-line no-console
-            console.log(error)
-        })
-
-    return prisma
+// 글로벌 객체에 Prisma Client 저장
+declare global {
+    var prismaGlobal: PrismaClient | undefined
 }
 
-declare const globalThis: {
-    prismaGlobal: PrismaClient
-} & typeof global
+let prisma: PrismaClient
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+if (process.env.NODE_ENV === 'production') {
+    // 프로덕션 환경에서는 인스턴스를 새로 생성
+    prisma = new PrismaClient()
+} else {
+    // 개발 환경에서는 글로벌 객체에 저장하여 Prisma 인스턴스가 여러 번 생성되지 않도록 처리
+    if (!global.prismaGlobal) {
+        global.prismaGlobal = new PrismaClient()
+    }
+    prisma = global.prismaGlobal
+}
+
+// 초기 데이터 설정 함수는 별도로 호출
+export const initializePrismaData = async () => {
+    try {
+        await createDefaultAdmin(prisma)
+        console.log('🚀  Prisma is ready')
+    } catch (error) {
+        console.log('❌  Failed to initialize Prisma data')
+        console.error(error)
+    }
+}
 
 export default prisma
-
-if (process.env.NODE_ENV !== 'production') {
-    globalThis.prismaGlobal = prisma
-}
