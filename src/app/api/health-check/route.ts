c993a0b4960f8a0b29f3d367a1fc2e00a251481e
eeennsu <diso592@naver.com>
2024-10-19 22:06:04 +0900

    test

--- src/app/api/health-check/route.ts
+++ src/app/api/health-check/route.ts
@@ -2,5 +2,6 @@ import { NextResponse } from 'next/server'
 
 // Health check
 export const GET = async () => {
+    console.log('postgresql db url', process.env.DATABASE_URL)
     return NextResponse.json({ status: 'ok!' })
 }
