c0c4173d534d69cb4b1cbfc86da60a3333d52d2d
eeennsu <diso592@naver.com>
2025-02-09 14:51:33 +0900

    feat: Implement icon in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
     metadataBase: new URL(SITE_URL),
     title: '방은수 블로그',
     description: '프론트엔드 개발자 방은수의 회고 블로그입니다.',
+    icons: {
+        icon: '/favicon.ico',
+    },
     authors: [
         {
             name: '방은수',
@@ -37,7 +40,7 @@ export const metadata: Metadata = {
         url: SITE_URL,
         title: '방은수 - 프론트엔드 개발자 블로그',
         description: 'Next.js 와 Typescript로 개발을 하는 프론트엔드 개발자 방은수의 블로그입니다.',
-        locale: 'ko_KR'
+        locale: 'ko_KR',
     },
     keywords:
         '프론트엔드 개발자, 방은수, 이력서, 포트폴리오, 블로그, Bang Eunsu, React.js, React, Next.js, Next, Typescript, ts, Frontend Developer, Resume, Portfolio',
